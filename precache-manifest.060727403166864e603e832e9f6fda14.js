2280712c278730e4a35043993b69dd30c4d2e765
shummi7 <sharmila16moorthi@gmail.com>
2023-03-15 14:02:51 -0500

    Updates

---
+++ precache-manifest.060727403166864e603e832e9f6fda14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "502d1c1b4b818da080e2",
+    "url": "/quizapp-frontend/static/css/main.1e7fb325.chunk.css"
+  },
+  {
+    "revision": "502d1c1b4b818da080e2",
+    "url": "/quizapp-frontend/static/js/main.502d1c1b.chunk.js"
+  },
+  {
+    "revision": "c713e826d116626ee653",
+    "url": "/quizapp-frontend/static/js/runtime~main.c713e826.js"
+  },
+  {
+    "revision": "9a86b04eade0e6122155",
+    "url": "/quizapp-frontend/static/css/2.3d7e7272.chunk.css"
+  },
+  {
+    "revision": "9a86b04eade0e6122155",
+    "url": "/quizapp-frontend/static/js/2.9a86b04e.chunk.js"
+  },
+  {
+    "revision": "7b2f313a6a3acae0b349f1e6c879a73f",
+    "url": "/quizapp-frontend/index.html"
+  }
+];
